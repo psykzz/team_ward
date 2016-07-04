95f876e2ffd64fad1c5783fb719663f1d4f55536
Matt Smith <msmith@riotgames.com>
2016-07-04 16:59:18 +0100

    Return game information
    Spaces over tabs and some other fixes.

--- teamward/RiotAPI.py
+++ teamward/RiotAPI.py
@@ -1,19 +1,32 @@
 from __future__ import print_function, absolute_import, division
 import requests
+import json
 
 
 class Client(object):
-	"""API for Riot Games API"""
-	def __init__(self, api_key):
-		self.api_key = api_key
-		self.api = requests.session()
+    """API for Riot Games API"""
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.api = requests.session()
+        self.api.params = {'api_key': self.api_key}
 
-	def find_summoner(self, region, summoner):
-		params = {'api_key': self.api_key}
-		res = self.api.get('https://{region}.api.pvp.net/api/lol/{region}'
-			'/v1.4/summoner/by-name/{name}'.format(
-				name=summoner, region=region), params=params).json()
-		return res.get(summoner) or res.get('status')
+    def find_summoner(self, region, summoner):
+        res = self.api.get(
+            'https://{region}.api.pvp.net/api/lol/{region}'
+            '/v1.4/summoner/by-name/{name}'.format(name=summoner,
+                                                   region=region)).json()
+        return res.get(summoner) or res.get('status')
 
-	def get_current_game(self, region, summoner):
-		return
+    def get_current_game(self, region, summoner):
+        res = self.api.get(
+            "https://{region}.api.pvp.net/"
+            "observer-mode/rest/consumer/getSpectatorGameInfo"
+            "/{shard}/{summoner_id}".format(summoner_id=summoner,
+                                            region=region,
+                                            shard=region.upper()+'1')).json()
+        return res
+
+
+if __name__ == '__main__':
+    test = Client('c43971d6-5eca-4aaf-bd49-6dde4ee8dbc6')
+    print(json.dumps(test.get_current_game('euw', test.find_summoner('euw', 'riottmx')['id'])))
--- teamward/app.py
+++ teamward/app.py
@@ -1,39 +1,36 @@
 from __future__ import print_function, absolute_import, division
 
 import os
+from teamward.RiotAPI import Client
 
 from flask import Flask, jsonify, g
 app = Flask(__name__)
 
-from teamward.RiotAPI import Client
-
 
 @app.before_request
 def before_request():
-	# Create the api client
-	g.api = Client(os.environ.get('API_KEY'))
+    # Create the api client
+    g.api = Client(os.environ.get('API_KEY'))
 
 
 @app.route('/gameinfo/by-player/<region>/<summoner>')
 def index(region, summoner):
 
-	summoner_info = g.api.find_summoner(region, summoner)
-	if summoner_info.get('status_code', 0) == 404:
-		return jsonify({'status': 'error', 
-			'error_code': 404, 
-			'reason': 'Summoner not found'})
+    summoner_info = g.api.find_summoner(region, summoner)
+    if summoner_info.get('status_code', 0) == 404:
+        return jsonify({'status': 'error',
+                        'error_code': 404,
+                        'reason': 'Summoner not found'})
 
-	game = g.api.get_current_game(region, summoner_info['id'])
-	if not game:
-		return jsonify({
-			'status': 'error', 
-			'error_code': 405, 
-			'reason': 'Summoner not in game'})
+    game = g.api.get_current_game(region, summoner_info['id'])
+    if not game:
+        return jsonify({
+            'status': 'error',
+            'error_code': 405,
+            'reason': 'Summoner not in game'})
 
-	return jsonify({
-
-		})
+    return jsonify(game)
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run(host='0.0.0.0')
+    app.debug = True
+    app.run(host='0.0.0.0')
